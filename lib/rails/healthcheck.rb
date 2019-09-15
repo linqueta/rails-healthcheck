49705cdec4f06e085fd5cfdae60321c6ec5be43e
linqueta <lincolnrodrs@gmail.com>
2019-09-15 03:32:02 -0300

    add project

---
+++ lib/rails/healthcheck.rb
@@ -0,0 +1,8 @@
+require "rails/healthcheck/version"
+
+module Rails
+  module Healthcheck
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rails/healthcheck/version.rb
@@ -0,0 +1,5 @@
+module Rails
+  module Healthcheck
+    VERSION = "0.1.0"
+  end
+end
