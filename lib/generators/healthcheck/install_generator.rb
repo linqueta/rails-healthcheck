02e905cff0242f62866c90950bf0813a0d71048f
linqueta <lincolnrodrs@gmail.com>
2020-03-15 14:20:00 -0300

    bump to 1.1.4

--- lib/generators/healthcheck/install_generator.rb
+++ lib/generators/healthcheck/install_generator.rb
@@ -19,9 +19,9 @@ module Healthcheck
             config.method = :get
 
             # -- Checks --
-            # config.add_check :database, -> { ActiveRecord::Base.connection.execute('select 1') }
-            # config.add_check :migrations,-> { ActiveRecord::Migration.check_pending! }
-            # config.add_check :cache, -> { Rails.cache.read('some_key') }
+            # config.add_check :database,     -> { ActiveRecord::Base.connection.execute('select 1') }
+            # config.add_check :migrations,   -> { ActiveRecord::Migration.check_pending! }
+            # config.add_check :cache,        -> { Rails.cache.read('some_key') }
             # config.add_check :environments, -> { Dotenv.require_keys('ENV_NAME', 'ANOTHER_ENV') }
           end
         HEALTHCHECK_INITIALIZER_TEXT
