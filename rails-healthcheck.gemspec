cddcda79b80de49e067a19d45486e572c2e4540f
Nick Campbell <661795+nickcampbell18@users.noreply.github.com>
2021-03-30 21:27:35 +0100

    Only depend on actionpack and railties for gem usage (#48)
    * Run healthcheck specs in UTC (avoid timezone offset issues)
    
    This test currently fails because my timezone is British Summer Time
    (UTC+1); the timestamp is parsed as being 1968, not 1969, and the tests
    fail. Using UTC (with the trailing `Z`) forces this to work wherever the
    test is run.
    
    * Only depend on actionpack and railties for gem usage
    
    This reduces the dependency surface area when included in Rails
    applications which aren't using all of the other dependencies (e.g.
    actioncable, activestorage, etc.)

--- rails-healthcheck.gemspec
+++ rails-healthcheck.gemspec
@@ -19,7 +19,8 @@ Gem::Specification.new do |spec|
 
   spec.required_ruby_version = '>= 2.5.1'
 
-  spec.add_dependency 'rails'
+  spec.add_dependency 'actionpack'
+  spec.add_dependency 'railties'
 
   spec.add_development_dependency 'bundler', '~> 2.0'
   spec.add_development_dependency 'rake', '~> 13.0'
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,8 +4,8 @@ require_relative 'boot'
 
 require 'rails'
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
+# require 'active_model/railtie'
+# require 'active_job/railtie'
 # require "active_record/railtie"
 # require "active_storage/engine"
 require 'action_controller/railtie'
